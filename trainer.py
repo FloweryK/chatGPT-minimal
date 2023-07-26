ba26a77a5fe31c4fc436a3b7a00e562a5e09b8c9
FloweryK <sunflower_94@kaist.ac.kr>
2023-07-27 01:34:10 +0900

    [refactor] show_sample, get_match detached from Trainer

--- trainer.py
+++ trainer.py
@@ -3,7 +3,37 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 from constant import *
-torch.set_printoptions(linewidth=10000)
+
+
+# TMP
+def show_sample(vocab, x_enc, x_dec_input, x_dec_target, predict):
+    def decode(name, sample, mask):
+        sample = sample[~mask].tolist()
+
+        print('\n' + name)
+        print(vocab.DecodeIds(sample))
+        print([vocab.DecodeIds([piece]) for piece in sample])
+        print(sample)
+
+    # extract logits from predict
+    predict = torch.argmax(predict, dim=1)
+
+    decode('x_enc', x_enc[0], x_enc[0].eq(PAD))
+    decode('x_dec_input', x_dec_input[0], x_dec_input[0].eq(PAD))
+    decode('x_dec_target', x_dec_target[0], x_dec_target[0].eq(PAD))
+    decode('predict', predict[0], x_dec_target[0].eq(PAD))
+
+
+def get_match(x_dec_target, predict):
+    # extract logits from predict
+    predict = torch.argmax(predict, dim=1)
+
+    target_flatten = x_dec_target.flatten()
+    predict_flatten = predict.flatten()
+    mask = ~target_flatten.eq(PAD)
+
+    correct = torch.eq(predict_flatten[mask], target_flatten[mask]).detach().cpu().tolist()
+    return correct
 
 
 class Trainer:
@@ -13,12 +43,6 @@ class Trainer:
         self.optimizer = optimizer
         self.vocab = vocab
         self.writer = SummaryWriter()
-    
-    def decodeSample(self, prefix, sample):
-        print(prefix)
-        print(self.vocab.DecodeIds(sample))
-        print([self.vocab.DecodeIds([piece]) for piece in sample])
-        print(sample, '\n')
     
     def run_epoch(self, epoch, dataloader, device, train=True):
         losses = []
@@ -49,23 +73,12 @@ class Trainer:
                 if train:
                     loss.backward()
                     self.optimizer.step()    
+
+                # show samples
+                show_sample(self.vocab, x_enc, x_dec_input, x_dec_target, predict)
                 
-                # sample
-                sample_enc_input = x_enc[0][~x_enc[0].eq(0)]
-                sample_dec_input = x_dec_input[0][~x_dec_input[0].eq(0)]
-                sample_dec_target = x_dec_target[0][~x_dec_target[0].eq(0)]
-                sample_predict = torch.argmax(predict, dim=1)[0][~x_dec_target[0].eq(0)]
-                self.decodeSample("\nenc_input", sample_enc_input.tolist())
-                self.decodeSample("dec_input", sample_dec_input.tolist())
-                self.decodeSample("dec_target", sample_dec_target.tolist())
-                self.decodeSample("dec_predict", sample_predict.tolist())
-                
-                # calculate performance
-                predict_flatten = torch.argmax(predict, dim=1).flatten()
-                target_flatten = x_dec_target.flatten()
-                mask = ~target_flatten.eq(0)
-                correct = torch.eq(predict_flatten[mask], target_flatten[mask]).detach().cpu().tolist()
-                match.extend(correct)
+                # get accuracy
+                match += get_match(x_dec_target, predict)
                 accuracy = np.mean(match) if match else 0
             
                 # update progress bar
